4d64bff41ec0de40a895486db97b5635d7e49abf
AGOKOLI <lilikers34@gmail.com>
2025-04-10 11:57:33 +0100

    Mise en place du message de bienvenue de l'assistance virtuelle

---
+++ src/component/WelcomMessage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface WelcomeMessageProps {
+  text: string;
+}
+
+const WelcomeMessage: React.FC<WelcomeMessageProps> = ({ text }) => (
+  <div className="text-center flex flex-col items-center">
+    <h2 className="text-2xl text-gray-500 font-medium mb-8">{text}</h2>
+  </div>
+);
+
+export default WelcomeMessage;
