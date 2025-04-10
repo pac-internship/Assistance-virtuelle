7f3881676d3dff12a14f5493c45c6775517b66f2
AGOKOLI <lilikers34@gmail.com>
2025-04-10 12:16:52 +0100

    Mises en place d'une logique conversationnel entre l'utilisateur et l'assistance virtuelle

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Header from "./component/Header";
-
+import Chatbot from "./component/chatBot";
 
 interface Message {
   id: number;
@@ -37,7 +37,17 @@ const App: React.FC = () => {
     <><div className="h-screen w-full flex flex-col relative transition-all duration-300">
       {/* En-tête */}
       <Header onHistoryToggle={toggleHistory} onNewChat={startNewChat} showHistory={showHistory} />
-
+       {/* Contenu du chat (InputBox) */}
+       <div
+          className={`flex justify-center items-center transition-all duration-300 ${
+            showHistory ? "w-[80%]" : "w-full"
+          }`}
+          id="chatbox"
+        >
+          <div className="w-[60%]">
+            <Chatbot messages={messages} setMessages={setMessages} />
+          </div>
+        </div>
      
     </div>
     </>
