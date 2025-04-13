41fdf1e7ba03e7cdc590cf98f7e79460466d9a12
AGOKOLI <lilikers34@gmail.com>
2025-04-13 18:32:38 +0100

    Création et Affichage du composant Header

--- chatbot-app/app/page.tsx
+++ chatbot-app/app/page.tsx
@@ -1,11 +1,48 @@
+'use client'
 
+import { Message } from '@/types/message'
+import React, { useState } from 'react'
+import Header from '../component/Header'
 
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      id: 1,
+      text: "Welcome! How can I help you today?",
+      sender: "bot",
+      timestamp: new Date().toISOString(),
+    },
+  ])
+  const [showHistory, setShowHistory] = useState<boolean>(false)
+
+  const toggleHistory = () => {
+    setShowHistory(!showHistory)
+  }
+
+  const startNewChat = () => {
+    setMessages([
+      {
+        id: 1,
+        text: "Hi, Comment puis-je t'aider aujourd'hui?",
+        sender: "bot",
+        timestamp: new Date().toISOString(),
+      },
+    ])
+  }
+
   return (
-    <div >
-     
-          
-          
+    <div className="h-screen w-full flex flex-col relative transition-all duration-300">
+      {/* En-tête */}
+      <Header
+        onHistoryToggle={toggleHistory}
+        onNewChat={startNewChat}
+        showHistory={showHistory}
+      />
+
+      {/* Exemple de contenu principal */}
+      <div className="flex-1 p-4">
+        <p>Hello world</p>
+      </div>
     </div>
-  );
+  )
 }
